ac7bb3558d7d5a58289d0bbb410049aa4fe987e1
zhongyalong <lanou@longge.local>
2016-07-27 15:14:44 +0800

    龙哥
    龙哥

--- js/cufon-replace.js
+++
@@ -1,3 +0,0 @@
-Cufon.replace('h2, h3, h4, .button, .support-phone', { fontFamily: 'Vegur', hover:true });
-Cufon.replace('.menu > li > a', { fontFamily: 'Vegur', hover:true });
-Cufon.replace('.footer-text', { fontFamily: 'Vegur', textShadow:'0 0 #fff', hover:true });
